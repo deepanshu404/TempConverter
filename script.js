ec5ef1103da3805d1b2989131411c1cd29e7ab99
deepanshu404 <deeps.farm.1@gmail.com>
2025-02-07 06:51:09 +0000

    Completed

---
+++ script.js
@@ -0,0 +1,63 @@
+// Selecting all necessary DOM elements
+const temperatureInput = document.getElementById('temperature');
+const fromUnit = document.getElementById('fromUnit');
+const toUnit = document.getElementById('toUnit');
+const convertButton = document.getElementById('converterButton');
+const resultDisplay = document.getElementById('result');
+
+function checkFields() {
+  if (temperatureInput.value && fromUnit.value && toUnit.value) {
+    convertButton.disabled = false;
+  } else {
+    convertButton.disabled = true;
+  }
+}
+
+temperatureInput.addEventListener('input', checkFields);
+fromUnit.addEventListener('change', checkFields);
+toUnit.addEventListener('change', checkFields);
+
+function convertTemperature(temp, from, to) {
+  if (from === to) {
+    return temp;
+  }
+
+  let convertedTemp;
+
+  if (from === 'Fahrenheit') {
+    if (to === 'Celsius') {
+      convertedTemp = (temp - 32) * 5 / 9;
+    } else if (to === 'Kelvin') {
+      convertedTemp = (temp - 32) * 5 / 9 + 273.15;
+    }
+  }
+
+  else if (from === 'Celsius') {
+    if (to === 'Fahrenheit') {
+      convertedTemp = (temp * 9 / 5) + 32;
+    } else if (to === 'Kelvin') {
+      convertedTemp = temp + 273.15;
+    }
+  }
+
+  else if (from === 'Kelvin') {
+    if (to === 'Fahrenheit') {
+      convertedTemp = (temp - 273.15) * 9 / 5 + 32;
+    } else if (to === 'Celsius') {
+      convertedTemp = temp - 273.15;
+    }
+  }
+
+  return convertedTemp;
+}
+
+convertButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const temperature = parseFloat(temperatureInput.value);
+  const from = fromUnit.value;
+  const to = toUnit.value;
+
+  const convertedValue = convertTemperature(temperature, from, to);
+  resultDisplay.textContent = `Converted Temperature: ${convertedValue.toFixed(2)} ${to}`;
+});
